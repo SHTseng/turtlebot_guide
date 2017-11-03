66fe23c47d3169b1e4dfdb928a97d1489ec9b049
shtseng <q253upng@gmail.com>
2017-11-04 01:57:46 +0800

    add unit testing with spawn robot using service
    Former-commit-id: 53c428a98110a2aa9536b2399e3f0d81500b290b

--- src/follower_tracker_node.cpp
+++ src/follower_tracker_node.cpp
@@ -22,16 +22,16 @@ public:
 
     // Create a ROS subscriber for the input point cloud and azimuthal angle
     sub_ = nh_.subscribe ("/guidance/camera/depth/points", 100,
-                                         &FollowerTrackerNode::trackCallback, this);
+                          &FollowerTrackerNode::trackCallback, this);
 
-    ir1_sub_ = nh_.subscribe("/guidance/ir_camera_mid/scan", 10,
-                                           &FollowerTrackerNode::IRCameraCallback, this);
+    ir1_sub_ = nh_.subscribe("/guidance/ir_camera_1/scan", 10,
+                             &FollowerTrackerNode::IRCameraCallback, this);
 
-    ir2_sub_ = nh_.subscribe("/guidance/ir_camera_l/scan", 10,
-                                           &FollowerTrackerNode::IRCamera2Callback, this);
+    ir2_sub_ = nh_.subscribe("/guidance/ir_camera_2/scan", 10,
+                             &FollowerTrackerNode::IRCameraTwoCallback, this);
 
     ir3_sub_ = nh_.subscribe("/guidance/ir_camera_3/scan", 10,
-                                           &FollowerTrackerNode::IRCamera3Callback, this);
+                             &FollowerTrackerNode::IRCameraThreeCallback, this);
 
     // Create a ROS publisher for the output point cloud and geometry pose
     pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("/guidance/output", 10);
@@ -68,12 +68,12 @@ public:
     ir_camera_1_msg_ = msg;
   }
 
-  void IRCamera2Callback(const gazebo_ir_camera_plugin::IRCamera &msg)
+  void IRCameraTwoCallback(const gazebo_ir_camera_plugin::IRCamera &msg)
   {
     ir_camera_2_msg_ = msg;
   }
 
-  void IRCamera3Callback(const gazebo_ir_camera_plugin::IRCamera &msg)
+  void IRCameraThreeCallback(const gazebo_ir_camera_plugin::IRCamera &msg)
   {
     ir_camera_3_msg_ = msg;
   }
@@ -82,42 +82,46 @@ private:
 
   double fusedSensorData()
   {
-    switch(current_state_)
-    {
-      case 0:
-        ROS_INFO_STREAM("Following");
-        break;
-      case 1:
-        ROS_INFO_STREAM("Not_Following");
-        break;
-      case 2:
-        ROS_INFO_STREAM("Lost");
-        break;
-    }
     double azi = 0.0;
-    if (ir_camera_1_msg_.azimuthal_angles.size() > 0)
-    {
-      azi = ir_camera_1_msg_.azimuthal_angles.front();
-      current_state_ = FOLLOWING;
-      ir_camera_1_msg_.azimuthal_angles.clear();
-      return azi;
-    }
+    ROS_INFO_STREAM(ir_camera_1_msg_.azimuthal_angles.size() << " " <<
+                    ir_camera_2_msg_.azimuthal_angles.size() << " " <<
+                    ir_camera_3_msg_.azimuthal_angles.size() << " ");
+//    switch(current_state_)
+//    {
+//      case 0:
+//        ROS_INFO_STREAM("Following");
+//        break;
+//      case 1:
+//        ROS_INFO_STREAM("Not_Following");
+//        break;
+//      case 2:
+//        ROS_INFO_STREAM("Lost");
+//        break;
+//    }
+//    if (ir_camera_1_msg_.azimuthal_angles.size() > 0)
+//    {
+//      azi = ir_camera_1_msg_.azimuthal_angles.front();
+//      current_state_ = FOLLOWING;
+//      ir_camera_1_msg_.azimuthal_angles.clear();
+//      return azi;
+//    }
 
-    if (ir_camera_2_msg_.azimuthal_angles.size() > 0 || ir_camera_3_msg_.azimuthal_angles.size() > 0)
-    {
-      current_state_ = NOT_FOLLOWING;
-      ir_camera_2_msg_.azimuthal_angles.clear();
-      ir_camera_3_msg_.azimuthal_angles.clear();
-      return azi;
-    }
+//    if (ir_camera_2_msg_.azimuthal_angles.size() > 0 || ir_camera_3_msg_.azimuthal_angles.size() > 0)
+//    {
+//      current_state_ = NOT_FOLLOWING;
+//      ir_camera_2_msg_.azimuthal_angles.clear();
+//      ir_camera_3_msg_.azimuthal_angles.clear();
+//      return azi;
+//    }
 
-    if (ir_camera_1_msg_.azimuthal_angles.size() == 0 &&
-        ir_camera_2_msg_.azimuthal_angles.size() == 0 &&
-        ir_camera_3_msg_.azimuthal_angles.size() == 0)
-    {
-      current_state_ = LOST;
-      return azi;
-    }
+//    if (ir_camera_1_msg_.azimuthal_angles.size() == 0 &&
+//        ir_camera_2_msg_.azimuthal_angles.size() == 0 &&
+//        ir_camera_3_msg_.azimuthal_angles.size() == 0)
+//    {
+//      current_state_ = LOST;
+//      return azi;
+//    }
+    return azi;
   }
 
   ros::NodeHandle nh_;
@@ -146,12 +150,5 @@ int main (int argc, char* argv[])
   ros::init (argc, argv, "follower_tracker_node");
   FollowerTrackerNode node;
 
-//  ros::spin();
-
-  while (ros::ok())
-  {
-    ros::Duration(0.5).sleep();
-    ros::spinOnce();
-//    ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
-  }
+  ros::spin();
 }
