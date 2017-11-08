a620f95b3d343fee7726c3fd18295c226633536d
shtseng <q253upng@gmail.com>
2017-11-08 16:11:10 +0800

    Fix the issue of spawning robot
    Former-commit-id: 0d1e74625b0d85088786cef44a4928673aa348d1

--- test/test_state_detect.cpp
+++ test/test_state_detect.cpp
@@ -1,19 +1,19 @@
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <ros/callback_queue.h>
 #include <std_srvs/Empty.h>
 
 #include <gazebo_msgs/GetWorldProperties.h>
 #include <gazebo_msgs/GetModelProperties.h>
+#include <gazebo_msgs/GetModelState.h>
 #include <gazebo_msgs/DeleteModel.h>
+#include <gazebo_msgs/ModelState.h>
 #include <gazebo_msgs/SpawnModel.h>
+#include <gazebo_msgs/SetModelState.h>
 
 #include <gazebo_ir_camera_plugin/IRCamera.h>
 
 #include <gtest/gtest.h>
-#include <tinyxml.h>
 
-bool has_new_image = false;
 int cnt = 0;
 
 void callBack(const gazebo_ir_camera_plugin::IRCameraConstPtr &msg)
@@ -21,13 +21,20 @@ void callBack(const gazebo_ir_camera_plugin::IRCameraConstPtr &msg)
   cnt = msg->azimuthal_angles.size();
 }
 
+bool inTolerance(const double &val,
+                 const double &ref,
+                 const double &range)
+{
+  return val < (ref+range) && val > (ref-range) ? true : false;
+}
+
 TEST(StateDetectTest, spawnRobot)
 {
   ros::NodeHandle nh("");
-//  ros::service::waitForService("/gazebo/pause_physics", -1);
-//  ros::ServiceClient pause_physics_client = nh.serviceClient<std_srvs::Empty>("/gazebo/pause_physics");
-//  std_srvs::Empty empty_srv;
-//  ASSERT_TRUE(pause_physics_client.call(empty_srv));
+  ros::service::waitForService("/gazebo/pause_physics", -1);
+  ros::ServiceClient pause_physics_client = nh.serviceClient<std_srvs::Empty>("/gazebo/pause_physics");
+  std_srvs::Empty empty_srv;
+  ASSERT_TRUE(pause_physics_client.call(empty_srv));
 
   ros::service::waitForService("/gazebo/spawn_urdf_model", -1);
   ASSERT_TRUE(ros::service::exists("/gazebo/spawn_urdf_model", false));
@@ -35,20 +42,15 @@ TEST(StateDetectTest, spawnRobot)
   ros::ServiceClient spawn_model_client = nh.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
   gazebo_msgs::SpawnModel spawn_model_srv;
   spawn_model_srv.request.model_name = "turtlebot";
-  spawn_model_srv.request.robot_namespace = "/follower";
+  spawn_model_srv.request.robot_namespace = "/test";
 
-//  TiXmlDocument xml_in(ros::package::getPath("turtlebot_guidance")+"/robot/turtlebot_follower.urdf");
-//  ASSERT_TRUE(xml_in.LoadFile());
-//  std::ostringstream stream;
-//  stream << xml_in;
-//  spawn_model_srv.request.model_xml = stream.str(); // load xml file
   std::string urdf_file;
-  ASSERT_TRUE(ros::param::get("/testing/robot_description", urdf_file));
+  ASSERT_TRUE(ros::param::get("/follower/robot_description", urdf_file));
   spawn_model_srv.request.model_xml = urdf_file;
 
   geometry_msgs::Pose current_pose;
   current_pose.position.x = 1.5;
-  current_pose.position.y = 0.0;
+  current_pose.position.y = 0.05;
   current_pose.position.z = 0.0;
   current_pose.orientation.w = 1.0;
   current_pose.orientation.x = 0.0;
@@ -64,13 +66,13 @@ TEST(StateDetectTest, spawnRobot)
   gazebo_msgs::GetWorldProperties world_properties;
   ASSERT_TRUE(check_model_client.call(world_properties));
   std::vector<std::string> model_names = world_properties.response.model_names;
-//  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot_follower") != model_names.end());
+  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot_follower") != model_names.end());
 
   ASSERT_TRUE(ros::service::waitForService("/gazebo/get_model_properties"));
   ros::ServiceClient check_model_srv = nh.serviceClient<gazebo_msgs::GetModelProperties>("/gazebo/get_model_properties");
   gazebo_msgs::GetModelProperties model_properties;
-  model_properties.request.model_name = "turtlebot";
-//  model_properties.request.model_name = "turtlebot_follower";
+//  model_properties.request.model_name = "turtlebot";
+  model_properties.request.model_name = "turtlebot_follower";
   ASSERT_TRUE(check_model_srv.call(model_properties));
 
   std::vector<std::string> body_names = model_properties.response.body_names;
@@ -78,9 +80,9 @@ TEST(StateDetectTest, spawnRobot)
   EXPECT_TRUE(std::find(body_names.begin(), body_names.end(), "ir_led_link") != body_names.end());
   EXPECT_TRUE(std::find(joint_names.begin(), joint_names.end(), "ir_led_joint") != joint_names.end());
 
-//  ros::service::waitForService("/gazebo/unpause_physics", -1);
-//  ros::ServiceClient unpause_physics_client = nh.serviceClient<std_srvs::Empty>("/gazebo/unpause_physics");
-//  ASSERT_TRUE(unpause_physics_client.call(empty_srv));
+  ros::service::waitForService("/gazebo/unpause_physics", -1);
+  ros::ServiceClient unpause_physics_client = nh.serviceClient<std_srvs::Empty>("/gazebo/unpause_physics");
+  ASSERT_TRUE(unpause_physics_client.call(empty_srv));
 
   SUCCEED();
 }
@@ -93,7 +95,7 @@ TEST(StateDetectTest, subscribeIRTopic)
   gazebo_msgs::GetWorldProperties world_properties;
   ASSERT_TRUE(check_model_client.call(world_properties));
   std::vector<std::string> model_names = world_properties.response.model_names;
-//  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot_follower") != model_names.end());
+  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot_follower") != model_names.end());
 //  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot") != model_names.end());
 
   ros::Subscriber ir_sub_ = nh.subscribe("/guidance/ir_camera_1/scan", 0, callBack);
@@ -103,39 +105,73 @@ TEST(StateDetectTest, subscribeIRTopic)
   }
   ASSERT_EQ(ir_sub_.getNumPublishers(), 1) << "IR Camera publisher does not exist";
 
-  ros::Duration(0.3).sleep();
+  ros::WallDuration(0.2).sleep();
+  cnt = 0;
   ros::spinOnce();
-  EXPECT_EQ(cnt, 1) << "Robot is not in the IR camera frame";
+  EXPECT_EQ(cnt, 2) << cnt << " Robots are not in the IR camera frame";
 }
 
-TEST(StateDetectTest, endTest)
+TEST(StateDetectTest, setRobotState)
 {
-  ros::shutdown();
+  ros::NodeHandle nh("");
+
+  ros::service::waitForService("/gazebo/get_model_state", 5.0);
+  ros::ServiceClient get_model_state_client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+  gazebo_msgs::GetModelState get_model_state_srv;
+  get_model_state_srv.request.model_name = "turtlebot_follower";
+  get_model_state_client.call(get_model_state_srv);
+  EXPECT_TRUE(inTolerance(get_model_state_srv.response.pose.position.x, 1.5, 0.1));
+
+  ros::Subscriber ir_sub = nh.subscribe("/guidance/ir_camera_1/scan", 0, callBack);
+  while (ir_sub.getNumPublishers() == 0)
+    ros::WallDuration(0.1).sleep();
+
+  ros::WallDuration(0.2).sleep();
+  int prev_cnt = cnt;
+  cnt = 0;
+  ros::spinOnce();
+  EXPECT_EQ(cnt, prev_cnt) << "Before moving the turtlebot: " << cnt;
+
+  ros::service::waitForService("/gazebo/set_model_state", 5.0);
+  ros::ServiceClient set_model_state_client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+  gazebo_msgs::SetModelState set_model_state_srv;
+  gazebo_msgs::ModelState model_state_msg;
+  model_state_msg.model_name = "turtlebot_follower";
+  model_state_msg.pose.position.x = 2.5;
+  set_model_state_srv.request.model_state = model_state_msg;
+  set_model_state_client.call(set_model_state_srv);
+
+  ros::WallDuration(0.2).sleep();
+  cnt = 0;
+  ros::spinOnce();
+  EXPECT_EQ(cnt, 1) << "After moving the turtlebot: " << cnt;
+
+  get_model_state_srv.request.model_name = "turtlebot_follower";
+  ASSERT_TRUE(get_model_state_client.call(get_model_state_srv));
+  EXPECT_TRUE(inTolerance(get_model_state_srv.response.pose.position.x, 2.5, 0.1));
 }
 
-//TEST(StateDetectTest, deleteRobot)
-//{
-//  ros::NodeHandle nh("");
-//  ASSERT_TRUE(ros::service::exists("/gazebo/delete_model", false));
-//  // make the service call to delete model
-//  ASSERT_TRUE(ros::service::waitForService("/gazebo/delete_model"));
-//  ros::ServiceClient delete_model_client = nh.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
-//  gazebo_msgs::DeleteModel delete_model;
+TEST(StateDetectTest, deleteRobot)
+{
+  ros::NodeHandle nh("");
+  ASSERT_TRUE(ros::service::waitForService("/gazebo/delete_model"));
+  ros::ServiceClient delete_model_client = nh.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
+  gazebo_msgs::DeleteModel delete_model;
 
-//  delete_model.request.model_name = "follower";
-//  delete_model_client.call(delete_model);
-//  ASSERT_TRUE(delete_model.response.success);
+  delete_model.request.model_name = "turtlebot";
+  delete_model_client.call(delete_model);
+  ASSERT_TRUE(delete_model.response.success);
+//  ros::WallDuration(0.1).sleep();
 
 //  ASSERT_TRUE(ros::service::waitForService("/gazebo/get_world_properties"));
 //  ros::ServiceClient check_model_client = nh.serviceClient<gazebo_msgs::GetWorldProperties>("/gazebo/get_world_properties");
 //  gazebo_msgs::GetWorldProperties world_properties;
 //  ASSERT_TRUE(check_model_client.call(world_properties));
 //  std::vector<std::string> model_names = world_properties.response.model_names;
-//  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "follower") == model_names.end());
+//  ASSERT_TRUE(std::find(model_names.begin(), model_names.end(), "turtlebot") == model_names.end());
 
-//  SUCCEED();
-//}
-
+//  ros::shutdown();
+}
 
 int main (int argc, char* argv[])
 {
