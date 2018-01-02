c5c46f6969f0835da2fda284acab6e6cc550e45d
shtseng <q253upng@gmail.com>
2018-01-02 23:45:35 +0800

    reset whole project into meta-package, implement turtlebot follower tracker
    Former-commit-id: 1226309821c57b2e67275bee103b73174c8e5db9 [formerly 9c98352c285f54a16a7f4058b10ecf883462b2fd] [formerly 1226309821c57b2e67275bee103b73174c8e5db9 [formerly 9c98352c285f54a16a7f4058b10ecf883462b2fd] [formerly 4f56e9e22e6660d67b5a15888991e6f2f7ab31b4]]
    Former-commit-id: 104ac372f13994fe7ede5b1dd537c3baf21730f1
    Former-commit-id: d52401e0c40136868337f47b0ef59f260f06bdd4
    Former-commit-id: ae41397530439e32be56b8cd6f110c2b31325a8d

---
+++ turtlebot_follower_tracker/include/turtlebot_follower_tracker/follower_tracker_ros.h
@@ -0,0 +1,56 @@
+#ifndef FOLLOWER_TRACKER_ROS_H
+#define FOLLOWER_TRACKER_ROS_H
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <boost/thread/mutex.hpp>
+#include <memory>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+#include <turtlebot_follower_tracker/follower_tracker.h>
+#include <rviz_visual_tools/rviz_visual_tools.h>
+
+namespace turtlebot_guide
+{
+class FollowerTrackerROS
+{
+public:
+
+  FollowerTrackerROS(ros::NodeHandle& n, ros::NodeHandle& pnh);
+
+  ~FollowerTrackerROS();
+
+private:
+
+  void depthCB(const sensor_msgs::PointCloud2ConstPtr& depth_msgs);
+
+  void connectCB(const ros::SingleSubscriberPublisher& pub);
+
+  void disconnectCB(const ros::SingleSubscriberPublisher& pub);
+
+  void publishBoundingBox(const geometry_msgs::Pose& msg);
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+
+  ros::Subscriber sub_;
+  ros::Publisher pose_pub_;
+  ros::Publisher cloud_pub_;
+  boost::mutex connect_mutex_;
+
+  ros::Time last_time_;
+
+  std::string mesh_name_;
+  std::shared_ptr<turtlebot_guide::FollowerTracker> ft_;
+  geometry_msgs::PoseStamped target_pose_;
+
+  tf::TransformListener tf_listener_;
+  tf::StampedTransform camera_base_tf_;
+
+  rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
+};
+}
+
+#endif // FOLLOWER_TRACKER_ROS_H
